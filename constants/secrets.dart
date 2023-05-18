a6df52e5e34bf3e1aae437cac31033f10daeb940
Fayazpk <94043611+Fayazpk@users.noreply.github.com>
2023-05-18 21:52:49 +0530

    Add files via upload

---
+++ constants/secrets.dart
@@ -0,0 +1,7 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyBREB5uHeVjK7eI5Bqm4VzifTjCmPeZT4c';
+
+  static const NO_IMG =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zsO-WB5sqXt2_4XGhgNqeecBwQ2dm2dTFcV4NBR0hBIK9nlKRuIz8HUwEo-eEteJBm4&usqp=CAU";
+}
